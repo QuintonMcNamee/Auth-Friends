81b5f45c93732537d88808ce1262e5d7e44aeeb7
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-08-20 17:02:26 -0400

    step 1 done: created login form component and approuter component

---
+++ my-app/src/components/LoginForm.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+const LoginForm = () => {
+
+  // hooks
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState([]);
+
+  // event handlers
+  const handleUsername = e => {
+    setUsername(e.target.value);
+  }
+  const handlePassword = e => {
+    setPassword(e.target.value);
+  }
+  const handleSubmit = e => {
+    e.preventDefault();
+    setUser([...user, {username: username, password: password}]);
+    setUsername('');
+    setPassword('');
+  }
+  
+  useEffect(() => {
+    console.log(user);
+  }, [user])
+
+  return (
+    <form onSubmit={e => handleSubmit(e)}>
+      <input type='text' value={username} placeholder='Username' onChange={e => handleUsername(e)} />
+      <input type='text' value={password} placeholder='Password' onChange={e => handlePassword(e)} />
+      <button>Submit</button>
+    </form>
+  )
+}
+
+export default LoginForm;
