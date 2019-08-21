73bdd20cac53a2bac0d7081ec096325eb9dcb6a7
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-08-21 17:15:48 -0400

    added functionality to add friends

--- my-app/src/components/FriendsData.js
+++ my-app/src/components/FriendsData.js
@@ -5,6 +5,9 @@ import { axiosWithAuth } from '../utils/axiosWithAuth';
 const FriendsData = () => {
 
   const [friendsData, setFriendsData] = useState([]);
+  const [friendName, setFriendName] = useState('');
+  const [friendAge, setFriendAge] = useState('');
+  const [friendEmail, setFriendEmail] = useState('');
 
   useEffect(() => {
     axiosWithAuth()
@@ -18,11 +21,42 @@ const FriendsData = () => {
         });
   }, [])
 
+  // event handlers
+  const handleName = e => {
+    setFriendName(e.target.value);
+  }
+  const handleAge = e => {
+    setFriendAge(e.target.value);
+  }
+  const handleEmail = e => {
+    setFriendEmail(e.target.value);
+  }
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    axiosWithAuth().post('http://localhost:5000/api/friends', {id: new Date(), name: friendName, age: friendAge, email: friendEmail})
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   return (
     <div>
       {friendsData.map(item => (
-        <p key={item.id}>{item.name}</p>
+        <p key={item.id}>{item.name}--{item.age}--{item.email}</p>
       ))}
+      <br />
+      <div>
+        <form onSubmit={e => handleSubmit(e)}>
+          <input type='text' name='name' placeholder='Name' onChange={e => handleName(e)} />
+          <input type='text' name='age' placeholder='Age' onChange={e => handleAge(e)} />
+          <input type='text' name='email' placeholder='Email' onChange={e => handleEmail(e)} />
+          <button>Add Friend</button>
+        </form>
+      </div>
     </div>
   )
 }
